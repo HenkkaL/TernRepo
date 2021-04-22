52c96a5d8621a117363f1fc3f8d361bf9560ba5a
Henrik Lyra <henrik.lyra@stat.fi>
2021-04-22 20:30:09 +0300

    Poistettu lintutaulu

--- src/tern_app/lib/data/moor_db.dart
+++ src/tern_app/lib/data/moor_db.dart
@@ -9,12 +9,7 @@ class Spots extends Table {
   TextColumn get description => text().withLength(max: 255).nullable()();
 }
 
-class Birds extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get name => text().withLength(min: 1, max: 255)();
-}
-
-@UseMoor(tables: [Spots, Birds])
+@UseMoor(tables: [Spots])
 class TernDb extends _$TernDb {
   TernDb() : super(FlutterQueryExecutor.inDatabaseFolder(path: 'tern.db'));
   @override
@@ -29,14 +24,4 @@ class TernDb extends _$TernDb {
   Future<int> insertSpot(Spot spot) => into(spots).insert(spot);
   Future<bool> updateSpot(Spot spot) => update(spots).replace(spot);
   Future<int> deleteSpot(Spot spot) => delete(spots).delete(spot);
-
-  Future<List<Bird>> getBirds() => (select(birds)
-        ..orderBy([
-          (bird) => OrderingTerm(expression: bird.name, mode: OrderingMode.asc)
-        ]))
-      .get();
-
-  Future<int> insertBird(Bird bird) => into(birds).insert(bird);
-  Future<bool> updateBird(Bird bird) => update(birds).replace(bird);
-  Future<int> deleteBird(Bird bird) => delete(birds).delete(bird);
 }
